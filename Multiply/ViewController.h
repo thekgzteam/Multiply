3ae1b92fb5af2460f2d48d45ab65463cf0b1a17c
Edil Ashimov <edilashimov@Edils-MacBook-Pro.local>
2015-07-19 07:16:57 -0500

    Initial Commit

---
+++ Multiply/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Multiply
+//
+//  Created by Edil Ashimov on 7/19/15.
+//  Copyright (c) 2015 Edil Ashimov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
